31be908553fe1bc04c394a80aa2018cbbe2d76e1
johnnyeven_work <johnnyeven@gmail.com>
2014-02-25 15:25:04 +0800

    1

--- application/controllers/master/order_check.php
+++ application/controllers/master/order_check.php
@@ -57,7 +57,7 @@ class Order_check extends CI_Controller
 		{
 			$extension['like'] = array('account_nickname', $nickname);
 		}
-		
+		$parameter['funds_flow_dir'] = 'CHECK_IN';
 		$result = $this->mfunds->read($parameter, $extension);
 		
 		if(empty($result))
