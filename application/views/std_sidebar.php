42f3a5e351731296423f3fb55ad291d0bb21cdc5
johnnyeven_work <johnnyeven@gmail.com>
2014-02-27 15:37:46 +0800

    1

--- application/views/std_sidebar.php
+++ application/views/std_sidebar.php
@@ -18,6 +18,7 @@ $permissionArray = explode(',', $admin->permission_list);
         <?php if(in_array('master/send_mail', $permissionArray) || in_array('All', $permissionArray)): ?><li<?php if($page_name == 'master/send_mail'): ?> class="active"<?php endif; ?>><a href="<?php echo site_url('master/send_mail'); ?>">发送邮件</a></li><?php endif; ?>
         <?php if(in_array('master/account_manage', $permissionArray) || in_array('All', $permissionArray)): ?><li<?php if($page_name == 'master/account_manage'): ?> class="active"<?php endif; ?>><a href="<?php echo site_url('master/account_manage'); ?>">帐号管理</a></li><?php endif; ?>
         <?php if(in_array('master/order_check', $permissionArray) || in_array('All', $permissionArray)): ?><li<?php if($page_name == 'master/order_check'): ?> class="active"<?php endif; ?>><a href="<?php echo site_url('master/order_check'); ?>">订单查询</a></li><?php endif; ?>
+        <?php if(in_array('master/get_device_id', $permissionArray) || in_array('All', $permissionArray)): ?><li<?php if($page_name == 'master/get_device_id'): ?> class="active"<?php endif; ?>><a href="<?php echo site_url('master/order_check'); ?>">设备码查询</a></li><?php endif; ?>
       </ul>
     </li>
     <li class="submenu"> <a href="#"><i class="icon icon-envelope"></i><span>公告管理</span><span class="label label-important"><i class="icon icon-arrow-down"></i></span></a>
