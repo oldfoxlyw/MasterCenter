65e03fd96e0049d392e1809e260be62d1cedecdf
johnnyeven_work <johnnyeven@gmail.com>
2014-02-27 15:19:51 +0800

    1

---
+++ application/controllers/master/get_device_id.php
@@ -0,0 +1,55 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Get_device_id extends CI_Controller
+{
+	private $pageName = 'master/get_device_id';
+	private $user = null;
+
+	public function __construct()
+	{
+		parent::__construct ();
+		$this->load->model('utils/check_user', 'check');
+		$this->user = $this->check->validate();
+		$this->check->permission($this->pageName);
+	}
+
+	public function index()
+	{
+		$this->load->model('mserver');
+		$serverResult = $this->mserver->read();
+		for($i=0; $i<count($serverResult); $i++)
+		{
+			$server = json_decode($serverResult[$i]->server_ip);
+			$serverResult[$i]->server_port = LAN_PORT;
+			$serverResult[$i]->server_ip = $server[0]->lan;
+		}
+		
+		$data = array(
+			'admin'				=>	$this->user,
+			'page_name'			=>	$this->pageName,
+			'server_result'		=>	$serverResult
+		);
+		$this->render->render($this->pageName, $data);
+	}
+	
+	public function lists()
+	{
+		$this->load->model('utils/connector');
+		$this->load->model('utils/return_format');
+		
+		$ip = $this->input->post('serverIp');
+		$nickname = $this->input->post('nickname');
+		
+		if(!empty($ip) && !empty($nickname))
+		{
+			$parameter = array(
+					'nkm'		=>	$nickname
+			);
+			
+			$result = $this->connector->post($ip . '/ser_get_device_id', $parameter, FALSE);
+			echo $result;
+		}
+	}
+}
+
+?>
---
+++ application/views/master/get_device_id.php
@@ -0,0 +1,87 @@
+<div id="content">
+<!--breadcrumbs-->
+<div id="content-header">
+    <div id="breadcrumb"> <span id="btnSwitchSidebar" class="badge margin-left-5 pointer" title="Close Sidebar"><i class="icon-chevron-left"></i><span> 关闭侧边栏</span></span><a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> 首页</a></div>
+    <h1>查询设备码</h1>
+</div>
+<!--End-breadcrumbs-->
+
+<!--Action boxes-->
+<div class="container-fluid">
+<!--End-Action boxes-->    
+    <div class="row-fluid">
+        <div class="widget-box">
+          <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
+            <h5>搜索</h5>
+          </div>
+          <div class="widget-content nopadding">
+			<div id="messageContainer"></div>
+              <form action="" method="post" class="form-horizontal">
+                <div class="control-group">
+                    <label class="control-label">选择服务器</label>
+                    <div class="controls">
+                        <select id="serverIp" name="serverIp">
+                        <?php foreach($server_result as $server): ?>
+                            <option value="http://<?php echo $server->server_ip; ?>:<?php echo $server->server_port; ?>"><?php echo $server->server_name; ?></option>
+                        <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label">角色昵称</label>
+                    <div class="controls">
+                    	<input type="text" class="span8" id="nickname" name="nickname" placeholder="角色昵称" />
+                  	</div>
+                </div>
+                <div class="form-actions">
+                  <button id="btnSubmit" type="button" class="btn btn-success">搜索</button>
+                </div>
+              </form>
+          </div>
+        </div>
+    </div>
+    
+    <div class="row-fluid">
+        <div class="widget-box">
+          <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
+            <h5>设备码</h5>
+          </div>
+          <div id="deviceId" class="widget-content nopadding">
+          
+          </div>
+        </div>
+    </div>
+</div>
+</div>
+<link rel="stylesheet" href="<?php echo base_url('resources/css/select2.css'); ?>" />
+<script src="<?php echo base_url('resources/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/matrix.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/select2.min.js'); ?>"></script>
+<script src="<?php echo base_url('resources/js/matrix.popup_message.js'); ?>"></script> 
+<script src="<?php echo base_url('resources/js/jquery.dataTables.min.js'); ?>"></script>
+
+<script type="text/javascript">
+$(function() {
+	$("#serverIp").select2();
+    $("#btnSubmit").click(function() {
+		if($("#nickname").val() == "") {
+			alert("请填写角色昵称");
+			return false;
+		}
+		$.post("<?php echo site_url('master/get_device_id/lists'); ?>", {
+			"serverIp": $("#serverIp").val(),
+			"nickname": $("#nickname").val()
+		}, onData);
+	});
+});
+
+function onData(data) {
+	if(!data) {
+		return;
+	}
+	
+	var json = eval("(" + data + ")");
+	console.log(json);
+}
+</script>
