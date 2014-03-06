3923f59586c2fccf63b50f2f79b11b5a75be0713
johnnyeven_work <johnnyeven@gmail.com>
2014-03-06 22:28:16 +0800

    1

--- application/controllers/cronjobs/tools.php
+++ application/controllers/cronjobs/tools.php
@@ -18,6 +18,7 @@ class Tools extends CI_Controller
 
 		$server1 = '103';
 		$server2 = '104';
+		$server3 = '105';
 		
 		$db = $this->load->database('accountdb', TRUE);
 		$sql = "SELECT COUNT(*) AS `count` FROM `web_account` WHERE `server_id`='{$server1}'";
@@ -31,14 +32,23 @@ class Tools extends CI_Controller
 		$result = $query->row();
 		$count2 = $result->count;
 		$query->free_result();
+
+		$sql = "SELECT COUNT(*) AS `count` FROM `web_account` WHERE `server_id`='{$server3}'";
+		$query = $db->query($sql);
+		$result = $query->row();
+		$count3 = $result->count;
+		$query->free_result();
 		
 		$active1 = 1;
 		$active2 = 0;
+		$active3 = 0;
 
 		$productdb = $this->load->database('productdb', TRUE);
 		$sql = "INSERT INTO `server_balance` VALUES('{$server1}', {$count1}, {$active1})";
 		$productdb->query($sql);
 		$sql = "INSERT INTO `server_balance` VALUES('{$server2}', {$count2}, {$active2})";
+		$productdb->query($sql);
+		$sql = "INSERT INTO `server_balance` VALUES('{$server3}', {$count3}, {$active3})";
 		$productdb->query($sql);
 	}
 	
@@ -49,12 +59,14 @@ class Tools extends CI_Controller
 
 		$id1 = 7;
 		$id2 = 8;
+		$id3 = 9;
 		
 		$server1 = '103';
 		$server2 = '104';
+		$server3 = '105';
 		
 		$productdb = $this->load->database('productdb', TRUE);
-		$sql = 'SELECT * FROM `server_balance` WHERE `last_active` = 0';
+		$sql = 'SELECT * FROM `server_balance` WHERE `next_active` = 1';
 		$lastResult = $productdb->query($sql)->row();
 		$serverId = $lastResult->server_id;
 		
@@ -63,10 +75,14 @@ class Tools extends CI_Controller
 			$nertId = $id2;
 			$nextServer = $server2;
 		}
+		else if($serverId == $server2)
+		{
+			$nertId = $id3;
+			$nextServer = $server3;
+		}
 		else
 		{
 			$nertId = $id1;
-			$otherId = $id2;
 			$nextServer = $server1;
 		}
 
@@ -94,9 +110,9 @@ class Tools extends CI_Controller
 			);
 			$this->mserver->update($nertId, $parameter);
 			
-			$sql = 'UPDATE `server_balance` SET `last_active` = 0';
+			$sql = 'UPDATE `server_balance` SET `next_active` = 0';
 			$productdb->query($sql);
-			$sql = "UPDATE `server_balance` SET `last_active` = 1, `count`={$count} WHERE `server_id`='{$serverId}'";
+			$sql = "UPDATE `server_balance` SET `next_active` = 1, `count`={$count} WHERE `server_id`='{$serverId}'";
 			$productdb->query($sql);
 		}
 	}
