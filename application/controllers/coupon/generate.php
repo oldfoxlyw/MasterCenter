2ff50226a44ef2adeabf85257f91ce1e9181c1e4
johnnyeven_work <johnnyeven@gmail.com>
2014-05-08 11:40:29 +0800

    1

--- application/controllers/coupon/generate.php
+++ application/controllers/coupon/generate.php
@@ -34,7 +34,7 @@ class Generate extends CI_Controller
 
 		$comment = empty($comment) ? '' : $comment;
 
-		$chars = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z'];
+		$chars = array('1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z');
 
 		if(!empty($prefix) && !empty($count))
 		{
