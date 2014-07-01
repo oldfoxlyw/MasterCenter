07616ccc07c78bec1fbe7770815cb6c2fe6b2294
johnnyeven_work <johnnyeven@gmail.com>
2014-07-01 16:55:40 +0800

    1

--- application/controllers/master/send_mail.php
+++ application/controllers/master/send_mail.php
@@ -20,16 +20,8 @@ class Send_mail extends CI_Controller
 		for($i=0; $i<count($serverResult); $i++)
 		{
 			$server = json_decode($serverResult[$i]->server_ip);
-			if(intval($serverResult[$i]->account_server_id) >= 103 || intval($serverResult[$i]->account_server_id) <= 100)
-			{
-				$serverResult[$i]->server_port = '8091';
-				$serverResult[$i]->server_ip = $server[0]->ip;
-			}
-			else
-			{
-				$serverResult[$i]->server_port = LAN_PORT;
-				$serverResult[$i]->server_ip = $server[0]->lan;
-			}
+			$serverResult[$i]->server_port = '8089';
+			$serverResult[$i]->server_ip = $server[0]->lan;
 		}
 		
 		$data = array(
