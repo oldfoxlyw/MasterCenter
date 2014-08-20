4a03cb3c45964eb21e4b080868dc37c769234e94
johnnyeven_work <johnnyeven@gmail.com>
2014-08-20 10:45:14 +0800

    1

--- application/views/master/send_mail.php
+++ application/views/master/send_mail.php
@@ -195,10 +195,15 @@ function onPackData(data) {
 }
 
 function onData(data) {
-	if(data == '1') {
+	var json = eval('(' + data + ')');
+	if(json.success == '1'){
 		popupMessage("messageContainer", "success", "已成功发送");
 	} else {
-		popupMessage("messageContainer", "error", "发送失败");
+		if(json.no_role.length > 0) {
+			popupMessage("messageContainer", "error", "发送失败，角色不存在。");
+		} else {
+			popupMessage("messageContainer", "error", "发送失败");
+		}
 	}
 }
 </script>
