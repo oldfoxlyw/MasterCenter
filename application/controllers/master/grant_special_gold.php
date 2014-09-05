b61286f5199d2b141ce238518f3e85a1ce135443
johnnyeven_work <johnnyeven@gmail.com>
2014-09-05 14:38:40 +0800

    1

--- application/controllers/master/grant_special_gold.php
+++ application/controllers/master/grant_special_gold.php
@@ -49,7 +49,6 @@ class Grant_special_gold extends CI_Controller
 				'account_nickname'		=>	$nickname,
 				'server_id'				=>	$id
 			));
-			exit(json_encode($account));
 			if(!empty($account))
 			{
 				$account = $account[0];
