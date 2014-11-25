693f6cbe42a42a7870d292a97e9a667be35dd010
johnnyeven_work <johnnyeven@gmail.com>
2014-11-25 14:06:16 +0800

    1

--- application/views/permission_add.php
+++ application/views/permission_add.php
@@ -44,18 +44,48 @@
                               <td width="20%"><input id="administrators_add" name="administrators_add" value="administrators_add" type="checkbox"<?php if(in_array('administrators_add', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />添加/编辑管理员</td>
                             </tr>
                             <tr>
-                              <td><input id="master_config" name="master_config" class="header_checkbox" type="checkbox" /></td>
-                              <td><strong>GM工具</strong></td>
+                              <td rowspan="3"><input id="master_config" name="master_config" class="header_checkbox" type="checkbox" /></td>
+                              <td rowspan="3"><strong>GM工具</strong></td>
                               <td width="20%"><input id="master_grant_gold" name="master_grant_gold" value="master/grant_gold" type="checkbox"<?php if(in_array('master/grant_gold', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />发放金币</td>
                               <td width="20%"><input id="master_grant_special_gold" name="master_grant_special_gold" value="master/grant_special_gold" type="checkbox"<?php if(in_array('master/grant_special_gold', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />发放绿钻</td>
-                              <td width="20%"><input id="master_grant_pack" name="master_grant_pack" value="master/grant_pack" type="checkbox"<?php if(in_array('master/grant_pack', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />发放礼包</td>
-                              <td width="20%"><input id="master_account_manage" name="master_account_manage" value="master/account_manage" type="checkbox"<?php if(in_array('master/account_manage', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />帐号管理</td>
+                              <td width="20%"><input id="master_grant_item" name="master_grant_item" value="master/grant_item" type="checkbox"<?php if(in_array('master/grant_item', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                              发放道具</td>
+                              <td width="20%"><input id="master_send_mail" name="master_send_mail" value="master/send_mail" type="checkbox"<?php if(in_array('master/send_mail', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                              发送邮件</td>
+                            </tr>
+                            <tr>
+                              <td><input id="master_grant_retinue" name="master_grant_retinue" value="master/grant_retinue" type="checkbox"<?php if(in_array('master/grant_retinue', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                              添加随从</td>
+                              <td><input id="master_set_honor" name="master_set_honor" value="master/set_honor" type="checkbox"<?php if(in_array('master/set_honor', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                                增加角色荣耀</td>
+                              <td><input id="master_set_level" name="master_set_level" value="master/set_level" type="checkbox"<?php if(in_array('master/set_level', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                                设置角色等级</td>
+                              <td><input id="master_account_manage" name="master_account_manage" value="master/account_manage" type="checkbox"<?php if(in_array('master/account_manage', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                                帐号管理</td>
+                            </tr>
+                            <tr>
+                              <td><input id="master_order_check" name="master_order_check" value="master/order_check" type="checkbox"<?php if(in_array('master/order_check', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                              订单查询</td>
+                              <td><input id="master_get_device_id" name="master_get_device_id" value="master/get_device_id" type="checkbox"<?php if(in_array('master/get_device_id', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                                设备码查询</td>
+                              <td>&nbsp;</td>
+                              <td>&nbsp;</td>
                             </tr>
                             <tr>
                               <td><input id="master_config" name="master_config" class="header_checkbox" type="checkbox" /></td>
                               <td><strong>公告管理</strong></td>
                               <td width="20%"><input id="master_message" name="master_message" value="master/message" type="checkbox"<?php if(in_array('master/message', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />定时公告管理</td>
                               <td width="20%"><input id="master_message_add" name="master_message_add" value="master/message_add" type="checkbox"<?php if(in_array('master/message_add', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />发布定时公告</td>
+                              <td width="20%">&nbsp;</td>
+                              <td width="20%">&nbsp;</td>
+                            </tr>
+                            <tr>
+                              <td><input id="master_config" name="master_config" class="header_checkbox" type="checkbox" /></td>
+                              <td><strong>兑换码</strong></td>
+                              <td width="20%"><input id="coupon_generate" name="coupon_generate" value="coupon/generate" type="checkbox"<?php if(in_array('coupon/generate', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                              生成兑换码</td>
+                              <td width="20%"><input id="coupon_query" name="coupon_query" value="coupon/query" type="checkbox"<?php if(in_array('coupon/query', $permission_check) || in_array('All', $permission_check)): ?> checked="checked"<?php endif; ?> />
+                              查询兑换码</td>
                               <td width="20%">&nbsp;</td>
                               <td width="20%">&nbsp;</td>
                             </tr>
