7da369245efc8341974ab4c65a3290e0ec7e30c7
johnnyeven_work <johnnyeven@gmail.com>
2015-01-16 17:48:14 +0800

    1

--- application/views/master/unlock_dungeons.php
+++ application/views/master/unlock_dungeons.php
@@ -2,7 +2,7 @@
 <!--breadcrumbs-->
 <div id="content-header">
     <div id="breadcrumb"> <span id="btnSwitchSidebar" class="badge margin-left-5 pointer" title="Close Sidebar"><i class="icon-chevron-left"></i><span> 关闭侧边栏</span></span><a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> 首页</a></div>
-    <h1>设置等级</h1>
+    <h1>解锁所有副本</h1>
 </div>
 <!--End-breadcrumbs-->
 
